4ac8aff7f91fff7842a75625cae45f6f0a474ab2
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-07-05 15:33:30 -0400

    issue #20771: p12 progress

--- source/oauth2/node-datasource/routes/generate_key.js
+++ source/oauth2/node-datasource/routes/generate_key.js
@@ -29,16 +29,18 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
         console.log("oauth2client error ", arguments);
         res.send({isError: true, error: err});
       },
-      convertToP12 = function (attachmentFilename, publicKey, privateKey) {
+      convertToP12 = function (publicKey, privateKey) {
         var filenamePrefix = path.join(process.cwd(), "lib/private/temp_" + id),
           publicKeyFilename = filenamePrefix + "_public_key.pem",
           csrFilename = filenamePrefix + "_csr.pem",
           certFilename = filenamePrefix + "_cert.pem",
           privateKeyFilename = filenamePrefix + "_private_key.pem",
           p12Filename = filenamePrefix + ".p12",
-          csrExec = "openssl req -new -key %@ -out %@".f(publicKeyFilename, csrFilename),
+          attachmentFilename = publicKey.substring(0, publicKey.indexOf("-----END")) +
+            "-private_key.pem",
+          csrExec = "openssl req -new -key %@ -out %@".f(privateKeyFilename, csrFilename),
           certExec = "openssl x509 -req -in %@ -signkey %@ -out %@"
-            .f(csrFilename, publicKeyFilename, certFilename),
+            .f(csrFilename, privateKeyFilename, certFilename),
           p12Exec = "openssl pkcs12 -export -in %@ -inkey %@ -out %@"
             .f(certFilename, privateKeyFilename, p12Filename),
           p12contents;
@@ -48,18 +50,27 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
         async.series([
           function (callback) { fs.writeFile(publicKeyFilename, publicKey, callback); },
           function (callback) { fs.writeFile(privateKeyFilename, privateKey, callback); },
-          function (callback) { exec(p12Exec, callback); },
+          function (callback) {
+            var child = exec(csrExec, callback);
+            // blow through command-line questions
+            child.stdin.write("\n\n\n\n\n\n\n\n\n");
+          },
+          function (callback) { exec(certExec, callback); },
+          function (callback) {
+            var child = exec(p12Exec, callback);
+            child.stdin.write("notasecret\nnotasecret\n"); // XXX: not to stdin?
+          },
           function (callback) {
             fs.readFile(p12Filename, function (err, contents) {
               p12contents = contents;
               callback(err, contents);
             });
           },
-          function (callback) { fs.unlinkFile(publicKeyFilename, callback); },
-          function (callback) { fs.unlinkFile(privateKeyFilename, callback); },
-          function (callback) { fs.unlinkFile(csrFilename, callback); },
-          function (callback) { fs.unlinkFile(certFilename, callback); },
-          function (callback) { fs.unlinkFile(p12Filename, callback); }
+          function (callback) { fs.unlink(publicKeyFilename, callback); },
+          function (callback) { fs.unlink(privateKeyFilename, callback); },
+          function (callback) { fs.unlink(csrFilename, callback); },
+          function (callback) { fs.unlink(certFilename, callback); },
+          function (callback) { fs.unlink(p12Filename, callback); }
         ],
         function (err, results) {
           if (err) {
@@ -68,17 +79,17 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
           }
           console.log(results);
           res.attachment(attachmentFilename);
-          res.send(new Buffer(p12contents));
+          console.log("p12 is ", p12contents);
+          res.send(p12contents);
+          //res.send(new Buffer(p12contents));
         });
       },
       fetchSuccess = function (model, result) {
         var keypair = ursa.generatePrivateKey(),
-          privateKey = keypair.toPrivatePem(),
+          privateKey = keypair.toPrivatePem().toString(),
           publicKey = keypair.toPublicPem().toString(),
-          filename = publicKey.substring(0, publicKey.indexOf("-----END")) +
-            "-private_key.pem",
           saveSuccess = function (model, result) {
-            convertToP12(filename, publicKey, privateKey);
+            convertToP12(publicKey, privateKey);
           };
 
         // Cursory validation: this should be a jwt bearer and the
