b2f81803722419cf3ce95a0bc8acf9c7a2a6d9bf
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-07-08 11:35:29 -0400

    issue #20771: implemented revoke-token route

--- source/oauth2/node-datasource/routes/revoke_token.js
+++ source/oauth2/node-datasource/routes/revoke_token.js
@@ -5,13 +5,54 @@ regexp:true, undef:true, strict:true, trailing:true, white:true */
 (function () {
   "use strict";
 
+  var async = require("async");
+
+  // https://localhost/dev/oauth/revoke-token?token=id123
   /**
     Revoke an OAUTH2 token.
-   The token can be an access token or a refresh token. If the token is an access token and it has a corresponding refresh token, the refersh token will also be revoked. (? from Google)
+   The token can be an access token or a refresh token.
+   If the token is an access token and it has a corresponding refresh token,
+   the refersh token will also be revoked.
    */
   exports.revokeToken = function (req, res) {
-    var token = req.query.token;
+    var token = req.query.token,
+      coll = new SYS.Oauth2tokenCollection();
 
-    res.send({isError: true, message: "TODO: implement this route."});
+    coll.fetch({
+      query: {
+        parameters: [{
+          attribute: ["refreshToken", "accessToken"],
+          operator: "MATCHES",
+          value: token
+        }]
+      },
+      username: req.session.passport.user.username,
+      database: req.session.passport.user.organization,
+      success: function (coll, result) {
+        var deleteModel = function (model, callback) {
+          model.destroy({
+            success: function () {
+              callback();
+            },
+            error: function (err) {
+              callback(err);
+            }
+          });
+        };
+
+        async.each(coll.models, deleteModel, function (err) {
+          if (err) {
+            res.send({isError: true, error: err});
+            return;
+          }
+          res.send({message: "Token revoked."});
+
+        });
+
+      },
+      error: function (err) {
+        res.send({isError: true, error: err});
+      }
+    });
   };
 }());
