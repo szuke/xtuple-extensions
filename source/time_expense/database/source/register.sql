2198f0d885c602b385edfe1ee95083a4cf83c356
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-07-19 10:00:19 -0400

    issue #20874: fix extension name

--- source/time_expense/database/source/register.sql
+++ source/time_expense/database/source/register.sql
@@ -1,3 +1,3 @@
 -- register extension 
 
-select xt.register_extension('timeExpense', 'Time Expense Management extension', '/xtuple-extensions', '', 100);
+select xt.register_extension('time_expense', 'Time Expense Management extension', '/xtuple-extensions', '', 100);
