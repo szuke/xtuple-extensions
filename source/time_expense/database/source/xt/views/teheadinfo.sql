fd92844b278ed4de25e80deed499d7427d19e000
John Rogelstad <john@xtuple.com>
2013-09-04 14:35:06 -0400

    issue #21359: Swat

--- source/time_expense/database/source/xt/views/teheadinfo.sql
+++ source/time_expense/database/source/xt/views/teheadinfo.sql
@@ -5,9 +5,9 @@ select tehead_id, tehead_number, tehead_weekending, tehead_emp_id, tehead_status
   xt.te_total_hours(tehead_id) as total_expenses,
   xt.te_to_voucher(tehead_id) as to_voucher,
   xt.te_to_invoice(tehead_id) as to_invoice,
-  xt.te_invoiced_state as invoiced,
-  xt.te_vouchered_state as vouchered,
-  xt.te_posted_state as posted,
+  xt.te_invoiced_state(tehead_id) as invoiced,
+  xt.te_vouchered_state(tehead_id) as vouchered,
+  xt.te_posted_state(tehead_id) as posted,
   xt.te_invoiced_value(tehead_id) as invoiced_value,
   xt.te_invoiced_value(tehead_id) as vouchered_value,
   xt.te_posted_value(tehead_id) as posted_value
