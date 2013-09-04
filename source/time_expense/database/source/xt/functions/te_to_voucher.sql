f4639c1546a979e7f895b25d4827666841068d62
John Rogelstad <john@xtuple.com>
2013-09-04 16:14:56 -0400

    issue #21359: Qualify schema names.

--- source/time_expense/database/source/xt/functions/te_to_voucher.sql
+++ source/time_expense/database/source/xt/functions/te_to_voucher.sql
@@ -3,7 +3,7 @@ create or replace function xt.te_to_voucher(tehead_id integer) returns numeric s
            when (teitem_type='T' and teemp_contractor=true and teitem_vodist_id is null) then
              coalesce(teitem_empcost, te.calcRate(emp_wage, emp_wage_period)) * teitem_qty
            else 0 end)
-  from tehead
+  from te.tehead
     join emp on (tehead_emp_id=emp_id)
     left outer join te.teitem on (tehead_id=teitem_tehead_id)
     left outer join te.teemp on (tehead_emp_id=teemp_emp_id)
