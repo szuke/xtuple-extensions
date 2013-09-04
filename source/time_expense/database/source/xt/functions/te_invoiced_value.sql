2c061bef7f91be88035ec562df9ee935de07488e
John Rogelstad <john@xtuple.com>
2013-09-04 12:34:14 -0400

    issue #21359: Get rid of aggregation to improve performance.

---
+++ source/time_expense/database/source/xt/functions/te_invoiced_value.sql
@@ -0,0 +1,5 @@
+create or replace function xt.te_invoiced_value(tehead_id integer) returns numeric stable as $$
+  select sum(case when teitem_invcitem_id is not null
+    then currtobase(teitem_curr_id, teitem_total, current_date) else 0 end)
+  from teitem where teitem_tehead_id=$1
+$$ language sql;
