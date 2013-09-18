38c2e7f7f0c290420764734361ec3cebf1bb481c
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-09-18 13:16:50 -0400

    removed register sql

--- source/oauth2/database/source/register.sql
+++
@@ -1,1 +0,0 @@
-select xt.register_extension('oauth2', 'OAUTH2 Administration', '/xtuple-extensions', '', 100);
--- source/time_expense/database/source/register.sql
+++
@@ -1,3 +0,0 @@
--- register extension 
-
-select xt.register_extension('time_expense', 'Time Expense Management extension', '/xtuple-extensions', '', 100);
