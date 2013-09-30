ea2f1712ab18ee31bf71157c4ed7aa15deafe8a3
Steve Hackbarth <stephenhackbarth@gmail.com>
2013-09-30 12:19:07 -0400

    applied new strings convention

--- source/time_expense/client/en/strings.js
+++ source/time_expense/client/en/strings.js
@@ -1,76 +1,69 @@
-// ==========================================================================
-// Project:   XT Strings
-// Copyright: ©2011 OpenMFG LLC, d/b/a xTuple
-// ==========================================================================
-/*globals XT */
+/*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true,
+latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
+strict:true, trailing:true, white:true */
 
-// Place strings you want to localize here.  In your app, use the key and
-// localize it using "key string".loc().  HINT: For your key names, use the
-// english string with an underscore in front.  This way you can still see
-// how your UI will look and you'll notice right away when something needs a
-// localized string added to this file!
-//
+(function () {
+  "use strict";
 
-XT.stringsFor("en_US", {
+  exports.strings = {
+    // ********
+    // Labels
+    // ********
 
-  // ********
-  // Labels
-  // ********
+    "_approve": "Approve",
+    "_approved": "Approved",
+    "_byAccount": "by Account",
+    "_byCategory": "by Category",
+    "_billing": "Billing",
+    "_billable": "Billable",
+    "_cost": "Cost",
+    "_isContractor": "Contractor",
+    "_custPo": "Cust. PO#",
+    "_detail": "Detail",
+    "_expense": "Expense",
+    "_invoiced": "Invoiced",
+    "_isSpecifiedRate": "Specified Rate",
+    "_noInvoice": "No Invoice",
+    "_noVoucher": "No Voucher",
+    "_notUsed": "Not Used",
+    "_TE": "Time Expense",
+    "_tE": "Time Expense",
+    "_posted": "Posted",
+    "_projectBilling": "Project Billing",
+    "_rate": "Rate",
+    "_site": "Site",
+    "_task": "Task",
+    "_time": "Time",
+    "_timeExpense": "Time Expense",
+    "_unapprove": "Unapprove",
+    "_unitCost": "Unit Cost",
+    "_vouchered": "Vouchered",
+    "_weekOf": "Week Of",
+    "_workDate": "Work Date",
+    "_worksheet": "Worksheet",
+    "_worksheets": "Worksheets",
 
-  "_approve": "Approve",
-  "_approved": "Approved",
-  "_byAccount": "by Account",
-  "_byCategory": "by Category",
-  "_billing": "Billing",
-  "_billable": "Billable",
-  "_cost": "Cost",
-  "_isContractor": "Contractor",
-  "_custPo": "Cust. PO#",
-  "_detail": "Detail",
-  "_expense": "Expense",
-  "_invoiced": "Invoiced",
-  "_isSpecifiedRate": "Specified Rate",
-  "_noInvoice": "No Invoice",
-  "_noVoucher": "No Voucher",
-  "_notUsed": "Not Used",
-  "_TE": "Time Expense",
-  "_tE": "Time Expense",
-  "_posted": "Posted",
-  "_projectBilling": "Project Billing",
-  "_rate": "Rate",
-  "_site": "Site",
-  "_task": "Task",
-  "_time": "Time",
-  "_timeExpense": "Time Expense",
-  "_unapprove": "Unapprove",
-  "_unitCost": "Unit Cost",
-  "_vouchered": "Vouchered",
-  "_weekOf": "Week Of",
-  "_workDate": "Work Date",
-  "_worksheet": "Worksheet",
-  "_worksheets": "Worksheets",
+    // ********
+    // Messages
+    // ********
+    "_postWorksheetFor": "Post Time Sheet for ",
+    "_toProject": " to Project",
+    "_closeWorksheet?": "Are you sure you want to close the worksheet? This action can not be undone.",
 
-  // ********
-  // Messages
-  // ********
-  "_postWorksheetFor": "Post Time Sheet for ",
-  "_toProject": " to Project",
-  "_closeWorksheet?": "Are you sure you want to close the worksheet? This action can not be undone.",
+    // ********
+    // Privileges
+    // ********
 
-  // ********
-  // Privileges
-  // ********
-
-  "_accessPPMExtension": "Access PPM Extension",
-  "_canApprove": "Approve Worksheets",
-  "_canViewRates": "Can View Billing Rates",
-  "_maintainEmpCostAll": "Maintain All Employee Costs",
-  "_maintainTimeExpense": "Delete and Close Worksheets",
-  "_maintainTimeExpenseOthers": "Maintain All Worksheets",
-  "_maintainTimeExpenseSelf": "Maintain Personal Worksheets",
-  "_postTimeSheets": "Post Worksheets",
-  "_viewTimeExpenseHistory": "View Worksheet History",
-  "_allowInvoicing": "Invoice Worksheets",
-  "_allowVouchering": "Voucher Worksheets"
-
-});
+    "_accessPPMExtension": "Access PPM Extension",
+    "_canApprove": "Approve Worksheets",
+    "_canViewRates": "Can View Billing Rates",
+    "_maintainEmpCostAll": "Maintain All Employee Costs",
+    "_maintainTimeExpense": "Delete and Close Worksheets",
+    "_maintainTimeExpenseOthers": "Maintain All Worksheets",
+    "_maintainTimeExpenseSelf": "Maintain Personal Worksheets",
+    "_postTimeSheets": "Post Worksheets",
+    "_viewTimeExpenseHistory": "View Worksheet History",
+    "_allowInvoicing": "Invoice Worksheets",
+    "_allowVouchering": "Voucher Worksheets"
+  };
+}());
