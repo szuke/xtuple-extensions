5aa0bd7249581686788628e6704607ff8c77d272
John Rogelstad <john@xtuple.com>
2013-10-06 16:45:11 -0400

    issue #21178: Disallow deletion of projects or project tasks that have worksheets associated with them.

---
+++ source/time_expense/database/source/xm/javascript/project.sql
@@ -0,0 +1,29 @@
+select xt.install_js('XM','Project','xtte', $$
+  /* Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+     See www.xm.ple.com/CPAL for the full text of the software license. */
+
+(function () {
+  
+  if (!XM.Project) { XM.Project = {}; }
+  
+  XM.Project.isDispatchable = true;
+
+  /**
+    Determine if a project is referenced by worksheets
+
+    @param {String} Project number
+    @returns Boolean
+  */
+  XM.Project.worksheetUsed = function(projectNumber) {  
+    var sql = "select count(teitem_id) as used " +
+              "from prj " +
+              " join prjtask on prj_id=prjtask_prj_id " +
+              " join te.teitem on teitem_prjtask_id=prjtask_id " +
+              "where prj_number = $1;";
+    return plv8.execute(sql, [projectNumber])[0].used;
+  };
+
+}());
+
+$$);
+
