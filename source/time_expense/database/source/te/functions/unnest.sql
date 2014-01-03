08ecd9d218e29f09cadadb77288a2f2bca795321
Greg Pazo <gpazo@xtuple.com>
2014-01-03 16:44:58 -0500

    update copyright to 2014

--- source/time_expense/database/source/te/functions/unnest.sql
+++ source/time_expense/database/source/te/functions/unnest.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION te.unnest(anyarray)
   RETURNS SETOF anyelement AS
 $BODY$
--- Copyright (c) 1999-2013 by OpenMFG LLC, d/b/a xTuple.
+-- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 SELECT $1[i] FROM
     generate_series(array_lower($1,1),
