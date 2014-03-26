5abdeee306b0d8cbd801c2bdcc395bd552512886
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-03-26 14:34:51 -0400

    issue #23168: get rid of the directory entirely

--- source/oauth2/README.md
+++
@@ -1,2 +0,0 @@
-This shell of oauth is just in place until our installs have their registered
-extension pointed towards core. It can be deleted in v4.5.0.
--- source/oauth2/database/source/manifest.js
+++
@@ -1,8 +0,0 @@
-{
-  "name": "oauth2",
-  "version": "1.8.2",
-  "comment": "Old OAUTH2 Extension",
-  "loadOrder": 9999,
-  "dependencies": [],
-  "databaseScripts": []
-}
